f14598f7934cbd4c6fb142df1569563e015f1354
SirDavidIII <david548219@gmail.com>
2019-09-26 20:24:09 +0300

    Add Docker pipeline

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3-onbuild
+
+CMD [ "python", "./app.py" ]
+
+EXPOSE 5000
---
+++ scripts/deploy_dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+#docker tag $TRAVIS_REPO_SLUG:$TAG $DOCKER_USER/$TRAVIS_REPO_SLUG
+docker push $DOCKER_USER/$TRAVIS_REPO_SLUG:${TAG}
