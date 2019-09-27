c34a60aac3375040b1561b9caa811c6b0f77cf0a
David <34396308+SirDavidIII@users.noreply.github.com>
2019-09-27 15:29:50 +0200

    Update deploy_dockerhub.sh

--- scripts/deploy_dockerhub.sh
+++ scripts/deploy_dockerhub.sh
@@ -6,4 +6,4 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t lingvo-subtitles:$TAG .
-docker push $DOCKER_USER/lingvo-subtitles:${TAG}
+docker push $DOCKER_USER/lingvo-subtitles:$TAG
