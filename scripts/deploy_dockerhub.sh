51ab5c76b0e0b8d4f51c3bb0ca435a2ecb0d2d5e
David <34396308+SirDavidIII@users.noreply.github.com>
2019-09-27 15:35:47 +0200

    Changed Docker image tag

--- scripts/deploy_dockerhub.sh
+++ scripts/deploy_dockerhub.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t lingvo-subtitles:$TAG .
+docker build -f Dockerfile -t $DOCKER_USER/lingvo-subtitles:$TAG .
+
 docker push $DOCKER_USER/lingvo-subtitles:$TAG
