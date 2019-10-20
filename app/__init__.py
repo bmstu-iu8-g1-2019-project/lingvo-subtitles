bf0e03db38b6c4ec410f7341fe9ac19e10864e3e
David548219 <david548219@gmail.com>
2019-10-20 17:47:04 +0300

    Add MongoDB client

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 
+from pymongo import MongoClient
+
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+
+db = MongoClient().lingvo
 
 from . import views
